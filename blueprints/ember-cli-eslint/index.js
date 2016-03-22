2c5992b9ff1a9186a509ac07fb0c449ee487f864
Alex LaFroscia <alex@lafroscia.com>
2016-03-21 23:41:03 -0400

    Move eslint-config-ember to dependencies

--- blueprints/ember-cli-eslint/index.js
+++ blueprints/ember-cli-eslint/index.js
@@ -5,13 +5,5 @@ module.exports = {
     // this prevents an error when the entityName is
     // not specified (since that doesn't actually matter
     // to us
-  },
-
-  afterInstall: function () {
-    return this.addPackageToProject('eslint-config-ember', '^0.3.0');
-  },
-
-  afterUninstall: function () {
-    return this.removePackageFromProject('eslint-config-ember');
-  },
+  }
 };
