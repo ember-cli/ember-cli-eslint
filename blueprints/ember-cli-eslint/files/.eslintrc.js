b142586abd6694ed2acfe00b398408149e53c7ae
Jan Bobisud <me@bobisjan.com>
2016-08-03 15:23:39 +0200

    Resolve Ember Testing coding standard

--- blueprints/ember-cli-eslint/files/.eslintrc.js
+++ blueprints/ember-cli-eslint/files/.eslintrc.js
@@ -1,7 +1,5 @@
-var path = require('path');
-
 module.exports = {
   extends: [
-    require.resolve('ember-cli-eslint/coding-standard/ember-application.js')
+    require.resolve('ember-cli-eslint/coding-standard/ember-application')
   ]
 };
--- blueprints/ember-cli-eslint/files/tests/.eslintrc.js
+++ blueprints/ember-cli-eslint/files/tests/.eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: '../node_modules/ember-cli-eslint/coding-standard/ember-testing.js'
+  extends: require.resolve('ember-cli-eslint/coding-standard/ember-testing')
 };
