0c717c9e8ab84bed3b8dd7495dc0da354818bf37
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-08-26 12:14:06 +0200

    Use default ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,15 @@
 'use strict';
 
 module.exports = {
-  "extends": "eslint:recommended",
-  "parser": "babel-eslint",
-  "env": {
-    "es6": true
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
   },
-  "rules": {
-    "prefer-arrow-callback": 0,
-    "space-before-function-paren": [0, { "anonymous": "always", "named": "never" }],
-    "object-curly-spacing": 0,
-    "prefer-reflect": 0,
-    "newline-after-var": 0,
-    "arrow-spacing": 0,
-    "array-callback-return": 0,
-    "no-empty-function": 0,
-    "prefer-rest-params": 0
+  extends: 'eslint:recommended',
+  env: {
+    'browser': true
+  },
+  rules: {
   }
 };
