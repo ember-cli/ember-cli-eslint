5f6afe7b72f8785f777014026fadd7f755c251cc
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-08-26 11:54:04 +0200

    config/eslint-tests: Remove duplicate key

--- config/eslint-tests.js
+++ config/eslint-tests.js
@@ -10,7 +10,6 @@ module.exports = {
     "object-curly-spacing": [2, "always"],
     "prefer-reflect": 0,
     "newline-after-var": 0,
-    "arrow-spacing": 0,
     "array-callback-return": 0,
     "no-empty-function": 0,
     "prefer-rest-params": 0
