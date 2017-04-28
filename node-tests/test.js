69e25b10cd0d4231e5b1af165fe828f560dbb071
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-04-28 20:47:48 +0200

    Adjust expected console output

--- node-tests/test.js
+++ node-tests/test.js
@@ -17,17 +17,17 @@ describe('ember-cli-eslint', function() {
     return emberTest().then(function(result) {
       expect(result.error).to.not.exist;
       expect(result.stdout.match(/[^\r\n]+/g))
-        .to.contain('ok 1 PhantomJS 2.1 - ESLint - app.js: should pass ESLint')
-        .to.contain('ok 2 PhantomJS 2.1 - ESLint - controllers/thing.js: should pass ESLint')
-        .to.contain('ok 3 PhantomJS 2.1 - ESLint - helpers/destroy-app.js: should pass ESLint')
-        .to.contain('ok 4 PhantomJS 2.1 - ESLint - helpers/module-for-acceptance.js: should pass ESLint')
-        .to.contain('ok 5 PhantomJS 2.1 - ESLint - helpers/resolver.js: should pass ESLint')
-        .to.contain('ok 6 PhantomJS 2.1 - ESLint - helpers/start-app.js: should pass ESLint')
-        .to.contain('ok 7 PhantomJS 2.1 - ESLint - models/thing.js: should pass ESLint')
-        .to.contain('ok 8 PhantomJS 2.1 - ESLint - resolver.js: should pass ESLint')
-        .to.contain('ok 9 PhantomJS 2.1 - ESLint - router.js: should pass ESLint')
-        .to.contain('ok 10 PhantomJS 2.1 - ESLint - test-helper.js: should pass ESLint')
-        .to.not.contain('not ok 11 PhantomJS 2.1 - ESLint - unused.js: should pass ESLint');
+        .to.contain('ok 1 PhantomJS 2.1 - ESLint | app.js: should pass ESLint')
+        .to.contain('ok 2 PhantomJS 2.1 - ESLint | controllers/thing.js: should pass ESLint')
+        .to.contain('ok 3 PhantomJS 2.1 - ESLint | helpers/destroy-app.js: should pass ESLint')
+        .to.contain('ok 4 PhantomJS 2.1 - ESLint | helpers/module-for-acceptance.js: should pass ESLint')
+        .to.contain('ok 5 PhantomJS 2.1 - ESLint | helpers/resolver.js: should pass ESLint')
+        .to.contain('ok 6 PhantomJS 2.1 - ESLint | helpers/start-app.js: should pass ESLint')
+        .to.contain('ok 7 PhantomJS 2.1 - ESLint | models/thing.js: should pass ESLint')
+        .to.contain('ok 8 PhantomJS 2.1 - ESLint | resolver.js: should pass ESLint')
+        .to.contain('ok 9 PhantomJS 2.1 - ESLint | router.js: should pass ESLint')
+        .to.contain('ok 10 PhantomJS 2.1 - ESLint | test-helper.js: should pass ESLint')
+        .to.not.contain('not ok 11 PhantomJS 2.1 - ESLint | unused.js: should pass ESLint');
     })
   });
 
@@ -37,17 +37,17 @@ describe('ember-cli-eslint', function() {
     return emberTest().then(function(result) {
       expect(result.error).to.exist;
       expect(result.stdout.match(/[^\r\n]+/g))
-        .to.contain('ok 1 PhantomJS 2.1 - ESLint - app.js: should pass ESLint')
-        .to.contain('ok 2 PhantomJS 2.1 - ESLint - controllers/thing.js: should pass ESLint')
-        .to.contain('ok 3 PhantomJS 2.1 - ESLint - helpers/destroy-app.js: should pass ESLint')
-        .to.contain('ok 4 PhantomJS 2.1 - ESLint - helpers/module-for-acceptance.js: should pass ESLint')
-        .to.contain('ok 5 PhantomJS 2.1 - ESLint - helpers/resolver.js: should pass ESLint')
-        .to.contain('ok 6 PhantomJS 2.1 - ESLint - helpers/start-app.js: should pass ESLint')
-        .to.contain('ok 7 PhantomJS 2.1 - ESLint - models/thing.js: should pass ESLint')
-        .to.contain('ok 8 PhantomJS 2.1 - ESLint - resolver.js: should pass ESLint')
-        .to.contain('ok 9 PhantomJS 2.1 - ESLint - router.js: should pass ESLint')
-        .to.contain('ok 10 PhantomJS 2.1 - ESLint - test-helper.js: should pass ESLint')
-        .to.contain('not ok 11 PhantomJS 2.1 - ESLint - unused.js: should pass ESLint');
+        .to.contain('ok 1 PhantomJS 2.1 - ESLint | app.js: should pass ESLint')
+        .to.contain('ok 2 PhantomJS 2.1 - ESLint | controllers/thing.js: should pass ESLint')
+        .to.contain('ok 3 PhantomJS 2.1 - ESLint | helpers/destroy-app.js: should pass ESLint')
+        .to.contain('ok 4 PhantomJS 2.1 - ESLint | helpers/module-for-acceptance.js: should pass ESLint')
+        .to.contain('ok 5 PhantomJS 2.1 - ESLint | helpers/resolver.js: should pass ESLint')
+        .to.contain('ok 6 PhantomJS 2.1 - ESLint | helpers/start-app.js: should pass ESLint')
+        .to.contain('ok 7 PhantomJS 2.1 - ESLint | models/thing.js: should pass ESLint')
+        .to.contain('ok 8 PhantomJS 2.1 - ESLint | resolver.js: should pass ESLint')
+        .to.contain('ok 9 PhantomJS 2.1 - ESLint | router.js: should pass ESLint')
+        .to.contain('ok 10 PhantomJS 2.1 - ESLint | test-helper.js: should pass ESLint')
+        .to.contain('not ok 11 PhantomJS 2.1 - ESLint | unused.js: should pass ESLint');
     })
   });
 });
