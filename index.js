3bdd3185b289b85526797710140e835de27ea650
Esteban Arango Medina <marranoparael31@gmail.com>
2017-10-22 17:40:24 -0500

    Fix #213

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ module.exports = {
 
     return ESLint.create(tree, {
       testGenerator: this.options.testGenerator || this._testGenerator,
-      group: (this.options.group !== false) ? type : undefined,
+      group: (this.options.testGenerator && this.options.group !== false) ? type : undefined,
       extensions: this.options.extensions,
 
       options: {
