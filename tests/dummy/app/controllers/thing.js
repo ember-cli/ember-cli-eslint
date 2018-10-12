b037c0b6054d93ef99804e091f865d7dd943bf85
Gaurav Munjal <gaurav@peeriq.com>
2018-10-12 12:59:07 -0400

    run codemods

--- tests/dummy/app/controllers/thing.js
+++ tests/dummy/app/controllers/thing.js
@@ -1,6 +1,6 @@
-import Ember from 'ember';
+import Controller from '@ember/controller';
 /* eslint no-undef: 0 */
 someOtherLib();
 
-export default Ember.Controller.extend({
+export default Controller.extend({
 });
